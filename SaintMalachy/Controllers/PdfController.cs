3bb386fb1d2daa332c25dc20ba90820469a48e0b
rupesh kurvankattil <rupesh.rajagopalan@gmail.com>
2017-02-18 18:47:49 -0500

    Race for grace changes

---
+++ SaintMalachy/Controllers/PdfController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaintMalachy.Controllers
+{
+    public class PdfController : Controller
+    {
+        // GET: Pdf
+        public void Index()
+        {
+            string filePath = Server.MapPath("~/Content/pdf/Race for Grace Flyer.pdf");
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AppendHeader("Content-disposition", "attachment; filename=Race for Grace Flyer.pdf"); // open in a new window
+            Response.WriteFile(filePath);
+            //Response.OutputStream.Write()
+            Response.Flush();
+        }
+    }
+}
